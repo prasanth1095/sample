7e3644aa3378c43457a711dc821aad4bcfb01648
prasanth1095 <mprasanthprism@gmail.com>
2020-01-27 23:19:21 +0530

    feat: add function

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import  Game from './myGame';
 class App extends Component {
   render() {
-    return <h1>Hello, World</h1>;
+    return <div>
+      <Game/>
+      <Game />
+    </div>
   }
 }
 
